f85413ba80f126fd54524d239ac5430e34ec20d9
Anil Kadurka <anilkadurka@gmail.com>
2024-02-20 14:50:39 +0530

    added divide.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from addition import add
 
+from divide import divide
+
 print("This is Main File")
 
 a=int(input())
@@ -7,3 +9,7 @@ b=int(input())
 
 x=add(a,b)
 print(x)
+
+
+y=divide(x)
+print(y)
